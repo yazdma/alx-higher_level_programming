fa325e9f9dc3ddb0c50b0a6159c72b7cde4b3b91
yazidmaaouni <yazidmaaouni@email.com>
2024-01-04 10:32:27 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         print("{} arguments.".format(size))
 
     else:
-        print("{} argument:".format(size))
+        print("{} arguments:".format(size))
         print("{}: {}".format(size, arg[1]))
