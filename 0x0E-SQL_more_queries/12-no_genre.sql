272912f861935b87a552b7a2a45f87f0adf7c7b6
Yazid Maaouni <yazidmaaouni@gmail.com>
2024-03-18 18:51:39 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -7,5 +7,5 @@ tv_show_genres
 ON tv_shows.id =
 tv_shows_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC,
+ORDER BY tv_shows.title ASC
 tv_show_genres.genre_id ASC;
